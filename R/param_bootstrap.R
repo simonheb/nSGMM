5df02601e16883ec53a26ada6a39e5013cf0b1a6
simonheb <simon.h.hess@gmail.com>
2021-02-23 15:03:38 +0100

    oldestimate removed

--- R/param_bootstrap.R
+++ R/param_bootstrap.R
@@ -4,21 +4,15 @@ bootstrap_parameter_estimate<-function(par,bootstrapseed,optimizer,drawcommand,d
   
   datahash<-digest::digest(vdata)
   optimizerhash<-digest::digest(deparse(optimizer))
-  
-  dir.create("estimates")
-  dir.create(paste0("estimates/",optimizerhash,sep=""))
-  dir.create(paste0("estimates/",optimizerhash,"/",datahash,sep=""))
-  
+
   dir.create("estimates")
   dir.create(paste0("estimates/",datahash,sep=""))
   dir.create(paste0("estimates/",datahash,"/",bootstrapseed,sep=""))
   dir.create(paste0("estimates/",datahash,"/",bootstrapseed,"/",optimizerhash,sep=""))
 
-  storedestimates_file<-paste0("estimates/",optimizerhash,"/",datahash,"/boostrapestimates.R",sep="")
   storedestimates_file_new<-paste0("estimates/",datahash,"/",bootstrapseed,"/",optimizerhash,"/bootstrapestimate.Rda",sep="")
   
   callkey<-paste(sep = "",bootstrapseed,digest::digest(c(par,digest::digest(list(...)))))
-  
   #open file if exists, use empty list otherwise
   boostrapestimates<-list()
   #if it exists in new files
@@ -32,7 +26,7 @@ bootstrap_parameter_estimate<-function(par,bootstrapseed,optimizer,drawcommand,d
         boostrapestimates[[callkey]]<-NULL
         saveRDS(boostrapestimates,storedestimates_file_new)
       } else if (thisest$val>10) {
-        cat(thisest$val)
+        cat("warning, value is high:",thisest$val)
         cat("*")
       } else {
         cat("*")
